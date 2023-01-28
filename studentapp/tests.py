940c6c397a7a30217647e1cea9a0125049b66208
Div Loop <80190976+thedivloop@users.noreply.github.com>
2023-01-28 18:59:38 +0700

    added StudentappModelTest

--- studentapp/tests.py
+++ studentapp/tests.py
@@ -2,4 +2,8 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class 
+class StudentappModelTest(TestCase):
+    def test_studentapp_model_exists(self):
+        studentapp = Studentapp.objects.all()
+
+        self.assertEqual()
