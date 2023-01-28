4ed509a924197e2dacd8c98a982d2149e38a71d3
Div Loop <80190976+thedivloop@users.noreply.github.com>
2023-01-28 20:38:45 +0700

    Added first template

--- studentapp/tests.py
+++ studentapp/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Studentapp
+from http import HTTPStatus
 
 # Create your tests here.
 
@@ -17,3 +18,21 @@ class StudentappModelTest(TestCase):
         )
 
         self.assertEqual(str(studentapp),studentapp.title)
+
+class HomepageTest(TestCase):
+    def setUp(self) -> None:
+        Studentapp.objects.create(
+            title = "sample post test 1",
+            body = "uet mattis eget facilisis ante. Praesent rhoncus enim libero, sit amet rutrum arcu pretium quis. Aenean feugiat mi in lectus blandit, ac dapibus lacus vehicula. Aenean commodo, lectus et dapibus portt"
+        )
+
+        Studentapp.objects.create(
+            title = "sample post test 2",
+            body = "uet mattis eget facilisis ante. Praesent rhoncus enim libero, sit amet rutrum arcu pretium quis. Aenean feugiat mi in lectus blandit, ac dapibus lacus vehicula. Aenean commodo, lectus et dapibus portt"
+        )
+
+    def test_homepage_returns_correct_response(self):
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response,'posts/index.html')
+        self.assertEqual(response.status_code,HTTPStatus.OK)
